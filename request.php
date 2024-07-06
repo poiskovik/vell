f49d417105c4a99d4b3a7b7ae46b587d718c444e
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 22:59:52 +0300

    Update request.php

--- request.php
+++ request.php
@@ -3,9 +3,9 @@ require_once "vendor/autoload.php";
  
 use GuzzleHttp\Psr7\Request;
  
-$client->request('GET', '/status/500');
+$client->request('GET', '/');
 // Throws a GuzzleHttp\Exception\ServerException
 
-$res = $client->request('GET', '/status/500', ['http_errors' => false]);
+$res = $client->request('GET', '/', ['http_errors' => false]);
 echo $res->getStatusCode();
 echo "iiiiiiii";
