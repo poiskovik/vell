4765dfe839c331ab0689dd04f03ff1b901043907
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 22:06:28 +0300

    Update request.php

--- request.php
+++ request.php
@@ -1,16 +1,16 @@
 <?php
 $xml='id=1';
-/*
+
 $opts = array(
   "http" => array(
     "method" => "PUT",
     "header" => "Accept: application/xml\r\n",
     "content" => $xml
   )
-);*/
-
+);
+/*
 $context = stream_context_create($opts);
 $response = file_get_contents($url, false, $context);
 print_r($response);
-
+*/
 ?>
