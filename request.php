a69e841d7634cd9af52bbbf523728934f4789eb1
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 22:51:39 +0300

    Update request.php

--- request.php
+++ request.php
@@ -1,18 +1,7 @@
 <?php
 require_once "vendor/autoload.php";
  
-use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
  
-$client = new Client([
-    'base_uri' => 'http://5.35.101.235',
-]);
- 
-$response = $client->request('GET', '', [
-    'query' => [
-        'page' => '2',
-    ]
-]);
- 
-$body = $response->getBody();
-$arr_body = json_decode($body);
-print_r($arr_body);
+$request = new Request('PUT', 'http://5.35.101.235/put');
+$response = $client->send($request, ['timeout' => 2]);
