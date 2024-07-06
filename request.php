ab6418a63676587e0336fe30f9e922ed62514cc1
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 23:51:02 +0300

    Update request.php

--- request.php
+++ request.php
@@ -8,7 +8,7 @@ function Apost ($login,$pass,$email) {
 $ch = curl_init('http://5.35.101.235/');
 
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER,  array(      'Authorization: apikey '.$authToken,      'Content-Type: application/json'  );
+curl_setopt($ch, CURLOPT_HTTPHEADER,  array( 'Content-Type: application/json'  );
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HEADER, false);
