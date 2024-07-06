add0d006a506a06cb2f10ba6222d30a8e7df9549
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 23:45:50 +0300

    Update request.php

--- request.php
+++ request.php
@@ -1,8 +1,13 @@
 <?php 
 
+//Создание пользователя, вводятся данные - логин, пароль, email
+$post=Apost('jeims','qwert','adm@adm.ru');
+
+
+function Apost ($login,$pass,$email) {
 $ch = curl_init('http://5.35.101.235/');
-curl_setopt($ch, CURLOPT_PUT, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: OAuth '));
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('login='.$login,'pass='.$pass,'email='.$email));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HEADER, false);
@@ -11,4 +16,5 @@ curl_close($ch);
  
 //$res = json_decode($res, true);
 print_r($res);
+}
 ?>
