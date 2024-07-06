04211fe411d6ca4259156c63071b367fd3dca162
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-07 00:04:51 +0300

    Update request.php

--- request.php
+++ request.php
@@ -2,11 +2,12 @@
 
 
 $ch = curl_init('http://5.35.101.235/');
-curl_setopt($ch, CURLOPT_PUT, true);
+curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: OAuth '));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'yyyu=yjytjyt');//json_encode(array('login='.$login,'pass='.$pass,'email='.$email))
 $res = curl_exec($ch);
 curl_close($ch);
 
