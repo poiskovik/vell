44a7e1369752611766b55267dfe7319e35bc9ab4
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-07 00:30:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,11 @@ switch($request_method) {
             $user->name = $data->name;
             $user->pass = $data->pass;
             $user->email = $data->email;
-print_r($user);
+            $STH = $db->prepare("INSERT INTO users (name, pass, email) values (:name, :pass, :email)");
+$STH->execute($data);
+            
+            
+print_r($STH);
             if($user->create()) {
                 echo "1";
                 http_response_code(201);
