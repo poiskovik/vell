775604218abba84aacdebef3cb8f3661892dda13
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 22:39:35 +0300

    Update request.php

--- request.php
+++ request.php
@@ -3,7 +3,7 @@ require_once('vendor/autoload.php');
 
 $client = new GuzzleHttp\Client(['base_uri' => 'http://5.35.101.235/']);
 
-$response = $client->get('http://httpbin.org/get');
+$response = $client->get('http://5.35.101.235/');
 //$response = $client->request('GET', 'test');
 print_r($response);
 ?>
