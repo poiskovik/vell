67bf76e253f622c32d05b1aecb603a0c10d6105c
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 22:49:06 +0300

    Update request.php

--- request.php
+++ request.php
@@ -1,10 +1,18 @@
+<?php
+require_once "vendor/autoload.php";
  
-<?php 
-require_once('vendor/autoload.php');
-
-$client = new GuzzleHttp\Client(['base_uri' => 'http://5.35.101.235/']);
-
-//$response = $client->get('http://5.35.101.235/index.php');
-$response = $client->request('GET', 'test');
-var_dump($response);
-?>
+use GuzzleHttp\Client;
+ 
+$client = new Client([
+    'base_uri' => 'http://5.35.101.235',
+]);
+ 
+$response = $client->request('GET', '', [
+    'query' => [
+        'page' => '2',
+    ]
+]);
+ 
+$body = $response->getBody();
+$arr_body = json_decode($body);
+print_r($arr_body);
