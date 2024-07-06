ef9432bb1324e1f7513ed7510b604560b9abc12c
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 22:30:57 +0300

    Update request.php

--- request.php
+++ request.php
@@ -2,13 +2,13 @@
 require_once('vendor/autoload.php');
 
 $client = new \GuzzleHttp\Client();
-/*
-$response = $client->request('GET', 'https://example.com/vat/api/v1/status', [
+
+$response = $client->request('GET', 'http://5.35.101.235/', [
   'headers' => [
     'accept' => 'application/json',
   ],
 ]);
-*/
+
 //echo $response->getBody();
 echo "1";
 ?>
