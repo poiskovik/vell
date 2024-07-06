b1df9429fe63f5436df97ae4311ffbeb5bcacdf3
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-06 22:38:42 +0300

    Update request.php

--- request.php
+++ request.php
@@ -5,5 +5,5 @@ $client = new GuzzleHttp\Client(['base_uri' => 'http://5.35.101.235/']);
 
 $response = $client->get('http://httpbin.org/get');
 //$response = $client->request('GET', 'test');
-//print_r($response);
+print_r($response);
 ?>
