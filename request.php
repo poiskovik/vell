c223c2f558177f948301aa357ee8f37642b81fab
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-07 00:58:10 +0300

    Update request.php

--- request.php
+++ request.php
@@ -1,6 +1,21 @@
 <?php 
-
 $post=Apost('jeims','qwert','adm@adm.ru');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function Apost ($login,$pass,$email) {
 $ch = curl_init('http://5.35.101.235/');
@@ -12,8 +27,7 @@ curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('name'=>$login,'pass'=>$pass,'email'=>$email)));
 $res = curl_exec($ch);
 curl_close($ch);
-
-$res = json_decode($res, true);
 print_r($res);
+$res = json_decode($res, true);
 }
 ?>
