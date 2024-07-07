ddde22608ea6c1338dade77482e0f6421195fb6d
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-07 10:24:27 +0300

    Update User.php

--- User.php
+++ User.php
@@ -40,18 +40,18 @@ class User {
     }
 
     public function update() {
-        $query = "UPDATE " . $this->table_name . " SET name = :name, pass = :pass, email = :email WHERE id = :id";
+        $query = "UPDATE " . $this->table_name . " SET pass = :pass, email = :email WHERE name = :name";
         $stmt = $this->conn->prepare($query);
 
         $this->name = htmlspecialchars(strip_tags($this->name));
         $this->pass = htmlspecialchars(strip_tags($this->pass));
         $this->email = htmlspecialchars(strip_tags($this->email));
-        $this->id = htmlspecialchars(strip_tags($this->id));
+        //$this->id = htmlspecialchars(strip_tags($this->id));
 
         $stmt->bindParam(':name', $this->name);
         $stmt->bindParam(':pass', $this->pass);
         $stmt->bindParam(':email', $this->email);
-        $stmt->bindParam(':id', $this->id);
+        //$stmt->bindParam(':id', $this->id);
 
         if($stmt->execute()) {
             return true;
