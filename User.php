fd733c120a22c04380689e094cd7697ab055ef53
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-07 11:02:31 +0300

    Update User.php

--- User.php
+++ User.php
@@ -21,8 +21,8 @@ class User {
     public function auth() {
         $query = "SELECT id, email FROM " . $this->table_name." WHERE name=:name and pass=:pass";
         print_r($query);
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
+       /* $stmt = $this->conn->prepare($query);
+        $stmt->execute();*/
         return $stmt;
     }
     public function create() {
