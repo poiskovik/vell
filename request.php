ce4d09aa22f7bc6008369b3954c0aefd46b11319
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-07 10:47:26 +0300

    Update request.php

--- request.php
+++ request.php
@@ -3,9 +3,9 @@
 
 //$put=Aput('kolya4','4rfrasd6fgh','4ghroot@delo.ru'); //Обновление информации пользователя (имя,пароль,почта)
 
-$delete=Adelete('kolya3'); //Удаление пользователя (имя)
+//$delete=Adelete('kolya3'); //Удаление пользователя (имя)
 
-
+$auth=Aauth('kolya5','5fsefq11wert'); //Авторизация пользователя (имя,пароль)
 
 
 
@@ -54,6 +54,19 @@ function Adelete ($login) {
   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('name'=>$login)));
   $res = curl_exec($ch);
   curl_close($ch);
+  $res = json_decode($res, true);
+  print_r($res['message']);
+}
+function Aauth ($login) {
+  $ch = curl_init('http://5.35.101.235/');
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_HEADER, false);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(array('name'=>$login,'pass'=>$pass)));
+  $res = curl_exec($ch);
+  curl_close($ch);
+  print_r($res);
   $res = json_decode($res, true);
   print_r($res['message']);
 }
