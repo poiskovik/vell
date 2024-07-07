f626b42d5b2a65c38fa601f98370a765bbd08be6
poiskovik <25633212+poiskovik@users.noreply.github.com>
2024-07-07 17:28:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,13 @@ $database = new Database();
 $db = $database->getConnection();
 
 $user = new User($db);
-//print_r($_SERVER);
 $request_method = $_SERVER["REQUEST_METHOD"];
-print_r($request_method);
 switch($request_method) {
     case 'GET':
     $data = json_decode(file_get_contents("php://input"));
         if(!empty($data->name)) {
-$stmt = $user->read();
-//$row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt = $user->read();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
             print_r($row);
 
         }
